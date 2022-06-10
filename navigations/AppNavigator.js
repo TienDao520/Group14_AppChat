b063caf004b8c7e9fbd329495ed2131f05df645f
Tien Dao <tiendao5240@gmail.com>
2022-06-10 00:46:03 -0400

    Added Firebase Google sign in

---
+++ navigations/AppNavigator.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {View, Text, Platform, StyleSheet, Pressable, Alert} from 'react-native';
+//yarn add @react-navigation/native
+import {NavigationContainer} from '@react-navigation/native';
+//yarn add @react-navigation/native-stack
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import SignUpScreen from '../screens/SignUpScreen';
+import SignInScreen from '../screens/SignInScreen';
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigator(props) {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        headerMode="screen"
+        screenOptions={{
+          headerTintColor: Platform.OS === 'android' ? 'white' : 'blue',
+          headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? 'green' : '',
+          },
+        }}>
+        <Stack.Screen
+          name="SignInScreen"
+          component={SignInScreen}
+          options={{
+            headerTitle: () => (
+              <View style={styles.loginHeader}>
+                <Text style={styles.headerTitle}>Group Chat</Text>
+                <Text style={styles.headerTitle}>Group 14</Text>
+              </View>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="SignUpScreen"
+          component={SignUpScreen}
+          options={{
+            headerTitle: () => (
+              <View style={styles.loginHeader}>
+                <Text style={styles.headerTitle}>Group Chat</Text>
+                <Text style={styles.headerTitle}>Group 14</Text>
+              </View>
+            ),
+          }}
+        />
+
+        {/* <Stack.Screen
+          name="RentingInputScreen"
+          component={RentingInputScreen}
+          options={{
+            title: "Renting Input"
+          }}
+        /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  loginHeader: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 40,
+  },
+  headerContainer: {
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
+
+export default AppNavigator;
