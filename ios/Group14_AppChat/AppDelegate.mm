afc7d232752462a39eda810e0d3e9a0754fd7d7f
Tien Dao <tiendao5240@gmail.com>
2022-06-10 00:14:43 -0400

    Added Firebase setup

--- ios/Group14_AppChat/AppDelegate.mm
+++ ios/Group14_AppChat/AppDelegate.mm
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -16,6 +18,8 @@
 
 #import <react/config/ReactNativeConfig.h>
 
+
+
 @interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
   RCTTurboModuleManager *_turboModuleManager;
   RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
@@ -29,6 +33,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+  [FIRApp configure];
+  
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
